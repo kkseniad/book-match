84805422e03562d7f73d4c99bd1b40ded5d9ffde
Kseniia <kormalovak@gmail.com>
2025-12-02 13:09:08 -0600

    changed to double quotes to pass lint test

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
 
-  root to: 'pages#main'
+  root to: "pages#main"
 end
