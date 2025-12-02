f34bc249a6b42ace351fa52df476c38e95fdc768
Kseniia <kormalovak@gmail.com>
2025-12-02 15:49:27 -0600

    added associations

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rails8Template
+# TODO: set up application module name
+module BookMatch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
