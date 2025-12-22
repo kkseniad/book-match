e573df4fc63c5272482b6c6f50d0871d264074a7
Kseniia <kormalovak@gmail.com>
2025-12-22 12:08:13 -0600

    generate pundit app policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  include Pundit::Authorization
   skip_forgery_protection
 
+  def pundit_user
+    Current.user
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   helper_method :user_signed_in?
 
   private
 
   def user_signed_in?
     Current.session&.user.present?
+  end
+
+  def user_not_authorized
+    redirect_to root_path, alert: "You are not authorized to perform this action."
   end
 end
