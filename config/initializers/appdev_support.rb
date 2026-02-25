539462209de395c91c06e06cb1fac4cc5075a708
Kseniia <kormalovak@gmail.com>
2026-02-25 12:38:59 -0600

    remove AppdevSupport initializer from production

--- config/initializers/appdev_support.rb
+++ config/initializers/appdev_support.rb
@@ -1,7 +1,9 @@
-AppdevSupport.config do |config|
-  config.action_dispatch = true
-  config.active_record   = true
-  config.pryrc           = :minimal
+if Rails.env.development?
+  AppdevSupport.config do |config|
+    config.action_dispatch = true
+    config.active_record   = true
+    config.pryrc           = :minimal
+  end
+  
+  AppdevSupport.init
 end
-
-AppdevSupport.init
